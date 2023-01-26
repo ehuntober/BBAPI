0559a3c05963da6310e2c12d763f3d849f30e75a
ehuntober <ehuntercrypto@gmail.com>
2023-01-26 22:14:03 +0000

    urls file modified

--- todos/urls.py
+++ todos/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from .views import ListTodo, DetailTodo
 
+#urlpattern
+
 urlpatterns = [
     path('<int:pk>/', DetailTodo.as_view(), name="todo_detail"),
     path('',ListTodo.as_view(),name='todo_list')
 ]
+
+
